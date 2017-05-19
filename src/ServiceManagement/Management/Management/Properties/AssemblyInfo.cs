214e5602fe72fdada143e6a39ee7efd375ea3586
bganapa <bganapa@microsoft.com>
2017-05-18 23:00:48 -0700

    updating to 4.1.3 version for signing with sha256

--- src/ServiceManagement/Management/Management/Properties/AssemblyInfo.cs
+++ src/ServiceManagement/Management/Management/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure infrastructure.")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.2.1")]
+[assembly: AssemblyFileVersion("4.1.3")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
