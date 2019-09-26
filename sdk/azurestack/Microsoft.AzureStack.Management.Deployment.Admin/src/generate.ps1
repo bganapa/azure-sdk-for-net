7dbe4ae6bdd7d02c20906e7becdd3e8bd3e82f60
Bala Ganapathy <bganapa@microsoft.com>
2019-09-26 10:14:42 -0700

    Deployment Admin SDK Initial version

---
+++ sdk/azurestack/Microsoft.AzureStack.Management.Deployment.Admin/src/generate.ps1
@@ -0,0 +1,1 @@
+Start-AutoRestCodeGeneration -ResourceProvider "azsadmin/resource-manager/deploy" -AutoRestVersion "latest" -SpecsRepoBranch "drpadmin" -SdkRootDirectory "$PSScriptRoot" -SpecsRepoFork "viananth" -SdkRepoRootPath "D:\github\azure-sdk-for-net"
