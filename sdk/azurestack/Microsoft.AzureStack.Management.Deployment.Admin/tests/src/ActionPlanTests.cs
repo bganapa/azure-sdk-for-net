774138ff569b8587506c27e959c3669565873dd1
Bala Ganapathy <bganapa@microsoft.com>
2019-10-11 09:44:35 -0700

    adding tests

---
+++ sdk/azurestack/Microsoft.AzureStack.Management.Deployment.Admin/tests/src/ActionPlanTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.Deployment.Admin;
+using Microsoft.AzureStack.Management.Deployment.Admin.Models;
+using System;
+using Xunit;
+
+namespace Deployment.Tests.src
+{
+    public class ActionPlanTests : DeploymentTestBase
+    {
+        [Fact]
+        public void TestUpdateEncruption() {
+            RunTest((client) =>
+            {
+                client.ActionPlans.Get("yes");
+            });
+        }
+    }
+}
---
+++ sdk/azurestack/Microsoft.AzureStack.Management.Deployment.Admin/tests/src/Helpers/DeploymentTestBase.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.Deployment.Admin;
+using Xunit;
+
+namespace Deployment.Tests
+{
+
+    public class DeploymentTestBase : AzureStackTestBase<DeploymentAdminClient>
+    {
+        public DeploymentTestBase()
+        {
+            // Empty
+        }
+
+        protected override void ValidateClient(DeploymentAdminClient client)
+        {
+            // validate creation
+            Assert.NotNull(client);
+
+            // validate objects
+            //Assert.NotNull(client.SubscriberUsageAggregates);
+            Assert.NotNull(client.SubscriptionId);
+        }
+    }
+}
